11f7e2eb9d16ba36b9e68defaf96e4d914514c35
lwj <lee@lwjs-MacBook-Pro.local>
2018-04-26 15:06:09 +0800

    6

---
+++ checkcheckcheck/your answer/20180425_CountLetter.py
@@ -0,0 +1,24 @@
+def checkio(text):
+
+    text=text.strip()
+    text= text.lower()
+    textlist=
+
+    print textlist
+
+
+checkio("Hello, Word!")
+
+'''
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
+'''
