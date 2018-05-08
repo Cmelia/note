bff982236ff2dc89c40e74d276039208e1a27fd2
lwj <lee@lwjs-MacBook-Pro.local>
2018-05-08 14:46:02 +0800

    zhengze6 split

--- source/regular-expression/zhengze6.py
+++ source/regular-expression/zhengze6.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import re
-#需求：分组1得到每行链接中的文件名
+# 需求：分组1得到每行链接中的文件名
 test_str='''
 http://localhost.com/a/b/c/d/file1.txt
 https://localhost.com/a/b/file2long.jpg
@@ -15,10 +15,11 @@ pattern = re.compile(r'^.*/(.*$)')
 for w in test_str.split():
     print(pattern.match(w).groups()[0])
 
-#方法二
+# 方法二
 strlist=test_str.split()
 for s in strlist:
     dizhi=s.split('/',-1)
-    print dizhi[-1]
+    print dizhi[-1]s
 
+# 方法三
 print([x.split('/',-1)[-1] for x in test_str.split()])
